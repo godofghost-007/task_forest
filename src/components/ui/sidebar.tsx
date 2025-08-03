64c7fa784e6783edbd84fa236a944d8354f3cfa6
Aniruddh Gupta <aniruddh.gupta.666@gmail.com>
2025-08-03 12:38:05 +0000

    just remove the error causing part and set it back

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -569,7 +569,7 @@ const SidebarMenuButton = React.forwardRef<
         data-size={size}
         data-active={isActive}
         className={cn(sidebarMenuButtonVariants({ variant, size, className }))}
-        {...(props as any)}
+        {...props}
       >
         {children}
       </Comp>
