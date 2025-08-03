b007f252b6c01462a8c2e4d1c0bda805ed9aff5b
Aniruddh Gupta <aniruddh.gupta.666@gmail.com>
2025-08-03 12:02:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.li role

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,11 +1,12 @@
+
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
+import { initializeApp, getApp, getApps, type FirebaseOptions } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   projectId: "task-forest-bnkxy",
   appId: "1:298212702743:web:b8166a432c895f5913fa52",
   storageBucket: "task-forest-bnkxy.firebasestorage.app",
